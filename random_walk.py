e86cb04b1ab3d617694ad328a448c6c96071ff33
4bic <robyne.kiplangat@gmail.com>
2016-12-01 13:09:34 +0300

    choose direction and distance

--- random_walk.py
+++ random_walk.py
@@ -4,8 +4,36 @@ class RandomWalk():
     """class to generate  random walks."""
 
     def __init__(self, num_points=5000):
+        """Initialize attributes of a walk"""
         self.num_points = num_points
 
         #All walks start from (0, 0)
-        self.x_value = [0]
-        self.y_value = [0]
+        self.x_values = [0]
+        self.y_values = [0]
+
+    def fill_walk(self):
+        """Calculate all the points in the walk"""
+
+        #keep taking steps till desired length has been reached
+        while len(self.x_values) < self.num_points:
+            #decide which direction to take and how far to go
+            x_direction = choice([1, -1])
+            x_distance = choice([0, 1, 2, 3, 4])
+            x_step = x_direction * x_distance
+
+            y_direction = choice([1, -1])
+            y_distance = choice([0, 1, 2, 3, 4])
+            y_step = y_direction * y_distance
+
+            #reject moves that go nowhere
+            if x_step == 0 and y_step == 0:
+                continue
+
+            #calculate the next X and Y values
+            next_x = self.x_values[-1] + x_step
+            next_y = self.y_values[-1] + y_step
+
+            self.x_values.append(next_x)
+            self.y_values.append(next_y)
+
+            
