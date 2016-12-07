56a08168d35d030937a44e4e1297bcf6bc0dc337
4bic <robyne.kiplangat@gmail.com>
2016-12-07 18:57:33 +0300

    styling color themes with Pygal

--- world_population.py
+++ world_population.py
@@ -1,6 +1,7 @@
 import json
 import pygal
 from pygal.maps.world import World
+from pygal.style import RotateStyle
 from country_codes import get_country_code
 
 #load data onto a list
@@ -30,7 +31,8 @@ for cc, pop in cc_populations.items():
 #see how many countries are in each level
 print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
 
-wm = World()
+wm_style = RotateStyle('#336699')
+wm = World(style=wm_style)
 wm.title = 'World population in 2010, by Country'
 wm.add('0-10M', cc_pops_1)
 wm.add('10M-1Bn', cc_pops_2)
