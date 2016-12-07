be3b895132cf6ff2ba640fb5171e81d7cc7ed94f
4bic <robyne.kiplangat@gmail.com>
2016-12-07 18:54:08 +0300

    group countries by population size

--- world_population.py
+++ world_population.py
@@ -18,8 +18,24 @@ for pop_dict in pop_data:
         if code:
             cc_populations[code] = population
 
+#group countries into three population levels
+cc_pops_1, cc_pops_2, cc_pops_3 = {}, {}, {}
+for cc, pop in cc_populations.items():
+    if pop < 10000000:
+        cc_pops_1[cc] = pop
+    elif pop < 1000000000:
+        cc_pops_2[cc] = pop
+    else:
+        cc_pops_3[cc] = pop
+#see how many countries are in each level
+print(len(cc_pops_1), len(cc_pops_2), len(cc_pops_3))
+
 wm = World()
 wm.title = 'World population in 2010, by Country'
-wm.add('2010', cc_populations)
+wm.add('0-10M', cc_pops_1)
+wm.add('10M-1Bn', cc_pops_2)
+wm.add('>1Bn', cc_pops_3)
+
+
 
 wm.render_to_file('world_population.svg')
