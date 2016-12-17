be7e54580574542d10c9bf332692d90f48b0c854
4bic <robyne.kiplangat@gmail.com>
2016-12-17 08:14:18 +0300

    working with response dictionary

--- python_repos.py
+++ python_repos.py
@@ -6,7 +6,15 @@ r = requests.get(url)
 print("Status code: ", r.status_code)
 
 #store API responce in a variable
-responce_dict = r.json()
+response_dict = r.json()
+print("Total repositories :", response_dict['total_count'])
 
-#process results
-print(responce_dict.keys())
+#Explore information about the repositories
+repo_dicts = response_dict['items']
+print("Repositories returned:", len(repo_dicts))
+
+#Examine the first repository
+repo_dict = repo_dicts[0]
+print ("\nKeys:", len(repo_dict))
+for key in sorted(repo_dict.keys()):
+    print(key)
